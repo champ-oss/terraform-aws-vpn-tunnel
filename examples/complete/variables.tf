38a3db7a2f3c0bf98d75842e1fc8627d9fecdb7c
Phillip Langley <philliplangley@champmbplangley.lan>
2023-02-24 08:32:03 -0600

    updating variables

---
+++ examples/complete/variables.tf
@@ -0,0 +1,5 @@
+variable "git" {
+  description = "Name of the Git repo"
+  type        = string
+  default     = "terraform-aws-alert"
+}
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "lambda_function_arn" {
+  description = "function arn"
+  value       = module.vpn_tunnel_lambda.arn
+}
