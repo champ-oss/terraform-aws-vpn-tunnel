df0c940f97eece83ccc1e5d44bcede3a1557bf25
Phillip Langley <philliplangley@champmbplangley.lan>
2023-02-26 12:28:02 -0600

    testing updated changes

--- lambda.tf
+++ lambda.tf
@@ -6,12 +6,9 @@ module "vpn_tunnel_lambda" {
   runtime                        = "python3.9"
   handler                        = "vpn_tunnel_modify.lambda_handler"
   sync_image                     = false
-  enable_vpc                     = true
-  private_subnet_ids             = var.private_subnet_ids
   filename                       = data.archive_file.lambda_zip.output_path
   source_code_hash               = data.archive_file.lambda_zip.output_base64sha256
   reserved_concurrent_executions = 1
-  vpc_id                         = var.vpc_id
   environment = {
     VPN_CONNECTION_ID = var.vpn_connection_id
   }
--- variables.tf
+++ variables.tf
@@ -25,13 +25,3 @@ variable "lambda_policy" {
   description = "point to data.aws_iam_policy_document.custom.json"
   default     = null
 }
-
-variable "private_subnet_ids" {
-  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_subnet_group#subnet_ids"
-  type        = list(string)
-}
-
-variable "vpc_id" {
-  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group#vpc_id"
-  type        = string
-}
