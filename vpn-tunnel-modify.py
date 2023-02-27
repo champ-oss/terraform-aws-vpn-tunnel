51e10dd9fc976b303d347ca3eb28d419b61fd2a0
Phillip Langley <philliplangley@champmbplangley.lan>
2023-02-27 11:22:57 -0600

    update to include dpd timeout

--- vpn-tunnel-modify.py
+++ vpn-tunnel-modify.py
@@ -3,7 +3,6 @@
 import logging
 import boto3
 import os
-from random import *
 import json
 
 ec2 = boto3.client('ec2')
@@ -13,15 +12,14 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def modify_vpn_tunnel(vpn_connection_id: str, vpn_tunnel_outside_ip_address: str) -> None:
+def modify_vpn_tunnel(vpn_connection_id: str, vpn_tunnel_outside_ip_address: str, dpd_timeout: int) -> None:
     logger.info("modify vpn tunnel")
-    random_dpd_timeout = randint(30, 130)  # Pick a random number between 30 and 130.
 
     ec2.modify_vpn_tunnel_options(
         VpnConnectionId=vpn_connection_id,
         VpnTunnelOutsideIpAddress=vpn_tunnel_outside_ip_address,
         TunnelOptions={
-            'DPDTimeoutSeconds': random_dpd_timeout,
+            'DPDTimeoutSeconds': dpd_timeout,
         }
     )
     logger.info("modify vpn tunnel complete")
@@ -30,9 +28,10 @@ def modify_vpn_tunnel(vpn_connection_id: str, vpn_tunnel_outside_ip_address: str
 def lambda_handler(event, context):
     # env variables requirements
     vpn_connection_id = os.environ['VPN_CONNECTION_ID']
+    dpd_timeout = os.environ['DPD_TIMEOUT'] # usually set to 30 by default
 
     message = event['Records'][0]['Sns']['Message']
     parsed_message = json.loads(message)
     vpn_tunnel_outside_ip_address = parsed_message['Trigger']['Dimensions'][0]['value']
 
-    modify_vpn_tunnel(vpn_connection_id, vpn_tunnel_outside_ip_address)
+    modify_vpn_tunnel(vpn_connection_id, vpn_tunnel_outside_ip_address, dpd_timeout)
